e10ccdf6852ace2a49510d64228a6ce67fd608a0
Daniel Kim <danieldkimster@gmail.com>
2010-10-31 14:26:27 -0400

    initial check-in

---
+++ test/test_numericality.js
@@ -0,0 +1,176 @@
+require.paths.unshift('./lib');
+
+var sys = require('sys');
+var validator = require('validator');
+var _ = require('underscore')._;
+var assert = require('assert');
+var test_util = require('test-util');
+var async_testing = require('async_testing')
+  , wrap = async_testing.wrap
+  ;
+
+// if this module is the script being run, then run the tests:  
+if (module == require.main) {
+  test_util.run(__filename, suite);
+}
+    
+var suite = wrap({
+  suiteSetup: function(done) {
+    done();
+  },
+  setup: function(test, done) {
+    test.o = {};
+    test.validation_config = {
+      default_messages: {
+        numericality: {}
+      },
+      properties: {
+        p: { 
+          numericality: {}
+        }
+      }
+    };
+    done();
+  },
+  teardown: function(test, done) {
+    done();
+  },
+  suite: {
+    "only_integer": test_only_integer,
+    "test_even": test_even,
+    "test_odd": test_odd,
+    "test_comparisons": test_comparisons
+  },
+  suiteTeardown: function(done) {
+    done();
+  }  
+});
+
+module.exports = { 'Numericality tests': suite };
+
+function test_only_integer(test) {
+  test_num_type(test, 'only_integer', "*{{name}} must be an integer.*", [
+    ["a", true], ["b", true], [1.1, true], [2.2, true], 
+    [1, false], [2, false], [1.0, false], [2.0, false]
+  ]);
+  test.finish();
+}
+
+function test_even(test) {
+  test_num_type(test, 'even', "*{{name}} must be even.*", [ 
+    [1, true], [2, false], [3, true], [4, false],
+    [-1, true], [-2, false], [-3, true], [-4, false]
+  ]);
+  test.finish();
+}
+
+function test_odd(test) {
+  test_num_type(test, 'odd', "*{{name}} must be odd.*", [ 
+    [1, false], [2, true], [3, false], [4, true],
+    [-1, false], [-2, true], [-3, false], [-4, true]
+  ]);
+  test.finish();
+}
+
+function test_comparisons(test, comp_type, msg_tmpl, compare_to, val_should_error_tuples) {  
+  [
+    [
+      "greater_than", "*{{name}} must be greater than {{compare_to}}.*", 0, 
+      [
+        [-1, true], ["-1", true], [-0.1, true], ["-0.1", true], [0, true], 
+        ["0", true], [0.1, false], ["0.1", false], [1, false], ["1", false]
+      ]
+    ],
+    [
+      "greater_than_or_equal_to", 
+      "*{{name}} must be greater than or equal to {{compare_to}}.*", 0, 
+      [
+          [-1, true], ["-1", true], [-0.1, true], ["-0.1", true], [0, false], 
+          ["0", false], [0.1, false], ["0.1", false], [1, false], ["1", false]
+      ]
+    ],
+    [
+      "equal_to", "*{{name}} must be equal to {{compare_to}}.*", 0, 
+      [
+          [-1, true], ["-1", true], [-0.1, true], ["-0.1", true], [0, false], 
+          ["0", false], [0.1, true], ["0.1", true], [1, true], ["1", true]
+      ]
+    ],
+    [
+      "less_than", "*{{name}} must be less than {{compare_to}}.*", 0, 
+      [
+          [-1, false], ["-1", false], [-0.1, false], ["-0.1", false], [0, true], 
+          ["0", true], [0.1, true], ["0.1", true], [1, true], ["1", true]
+      ]
+    ],
+    [
+      "less_than_or_equal_to", "*{{name}} must be less than or equal to {{compare_to}}.*", 0, 
+      [
+          [-1, false], ["-1", false], [-0.1, false], ["-0.1", false], [0, false], 
+          ["0", false], [0.1, true], ["0.1", true], [1, true], ["1", true]
+      ]
+    ]
+  ].forEach(function(test_config) {    
+    var comp_type = test_config[0], msg_tmpl = test_config[1],
+        compare_to = test_config[2], val_should_error_tuples = test_config[3];
+    test.validation_config = {
+      default_messages: {
+        numericality: {}
+      },
+      properties: {
+        p: { 
+          numericality: {}
+        }
+      }
+    };
+    test_num_type(test, comp_type, msg_tmpl, val_should_error_tuples, compare_to)
+  });
+  
+  test.finish();
+}
+
+function test_num_type(test, num_type, msg_tmpl, 
+    val_should_error_tuples, compare_to) {
+  var config = test.validation_config;
+  config.default_messages.numericality[num_type] = msg_tmpl;
+  config.properties.p.numericality[num_type] = 
+    compare_to == undefined ? true : compare_to;
+
+  test_val_should_error_tuples(val_should_error_tuples,
+    _test_should_error(test, config, 
+      interp_s(msg_tmpl, {name: 'P', compare_to: compare_to})),
+    _test_should_not_error(test, config));
+}
+
+function _test_should_error(test, config, msg) {
+  return function(val) {
+    test.o.p = val;
+    var errors = validator.validate(test.o, config);
+    test.ok(errors, "no errors returned.");
+    test.ok(errors.on('p'), "no errors on 'p'");
+    test.equal(1, errors.on('p').length, "should only have 1 error on 'p'");    
+    test.equal(msg, errors.on('p')[0]); 
+  }
+}
+
+function _test_should_not_error(test, config) {
+  return function(val) {
+    test.o.p = val;
+    var errors = validator.validate(test.o, config);
+    test.equal(true, !errors, "errors is a non-blank value.");
+  }
+}
+
+function interp_s(tmpl, values) {
+  _.each(values, function(v, k) { tmpl = tmpl.replace('{{' + k + '}}', v); });
+  return tmpl;
+}
+
+function test_val_should_error_tuples(val_should_error_tuples, test_should_error, test_should_not_error) {
+  _.each(val_should_error_tuples, function(val_should_error) {
+    var val = val_should_error[0], should_error = val_should_error[1];
+    if (should_error) test_should_error(val);
+    else test_should_not_error(val);
+  });
+}
+
---
+++ validator.js
@@ -0,0 +1,193 @@
+var _ = require('underscore')._;
+var sys = require('sys');
+require('inflection');
+
+var global_default_messages = {
+  required: "{{name}} is required.",
+  length: {
+    is_message: "{{name}} must be exactly {{compare_to}} characters.",
+    min: "{{name}} must be at least {{compare_to}} characters.",
+    max: "{{name}} must not exceed {{compare_to}} characters."
+  },
+  numericality: {
+    only_integer: "{{name}} must be an integer.",
+    greater_than: "{{name}} must be greater than {{compare_to}}",
+    greater_than_or_equal_to: "{{name}} must be greater than or equal to {{compare_to}}.",
+    equal_to: "{{name}} must be equal to {{compare_to}}.",
+    less_than: "{{name}} must be less than {{compare_to}}.",
+    less_than_or_equal_to: "{{name}} must be less than or equal to {{compare_to}}.",
+    odd: "{{name}} must be an odd number.",
+    even: "{{name}} must be an even number."    
+  },
+  format: {
+    pattern: "{{name}} is not formatted correctly."
+  }
+}
+
+var new_errors= exports.new_errors = function() {
+  var _errors = {};
+  return {
+    errors: function() {return _errors},
+    add: function(name, error) {
+      if (!error) return;
+      if (typeof error == 'string') {
+        var messages = _errors[name];
+        if (!messages) _errors[name] = messages = [];
+        messages.push(error);        
+      } else {
+        _errors[name] = error;
+      }
+    },
+    add_to_base: function(error) {
+      this.add('_base', error);
+    },
+    clear: function() {
+      _errors = {};
+    },
+    count: function() {
+      return this.size();
+    },
+    each: function(callback) {
+      _.each(_errors, function(error, name) {
+        callback(name, error);
+      })
+    },
+    is_empty: function() {
+      return _.isEmpty(_errors);
+    },
+    messages: function() {
+      return _.flatten(_.values(_errors));
+    },
+    is_invalid: function(name) {
+      var error = _errors[name];
+      if (!error) return false;
+      if (typeof error == 'array') return error.length > 0;
+      else return true;
+    },
+    length: function(){
+      return this.size();
+    },
+    on: function(name) {
+      return _errors[name];
+    }, 
+    on_base: function() {
+      return _errors['_base'];
+    },
+    size: function() {
+      return _.size(_errors);
+    },
+    toString: function() {
+      return sys.inspect(_errors);
+    }
+  };
+}
+
+var valid_funcs = {
+  required: function(val) {return val;},
+  length: {
+    is: function(val, compare_to) {return val.length == compare_to;},
+    max: function(val, compare_to) {return val.length <= compare_to; },
+    min: function(val, compare_to) {return val.length >= compare_to; }
+  },
+  numericality: {
+    only_integer: function(val) {return parseInt(val) == val;}, 
+    odd: function(val) {return Math.abs(val) % 2 == 1;}, 
+    even: function(val) {return val % 2 == 0;}, 
+    greater_than: function(val, compare_to) {return val > compare_to;},
+    greater_than_or_equal_to: function(val, compare_to) {return val >= compare_to;} ,
+    equal_to: function(val, compare_to) {return val == compare_to;} ,
+    less_than: function(val, compare_to) {return val < compare_to;} ,
+    less_than_or_equal_to: function(val, compare_to) {return val <= compare_to;} 
+  },
+  format: {
+    pattern: function(val, compare_to) {return val.match(compare_to);}
+  }
+}
+// we want to execute the validations in this order
+var validation_types = ['required', 'length', 'numericality', 'format'];
+
+var interpolation_scope_extractors = {
+  length: {
+    count: function(val) {return {count: val.length};},
+    is: function(val) {return {count: val.length};},
+    min: function(val) {return {count: val.length};},
+    max: function(val) {return {count: val.length};},
+  }
+};
+
+exports.validate = function validate(obj, config) {  
+  
+  var errors = new_errors();
+  var default_messages = config.default_messages || {};
+  _.without(validation_types, 'required').forEach(function(validation_type) {
+    if (!default_messages[validation_type]) default_messages[validation_type] = {};
+  });
+
+  function test_and_add_error_message(prop_name, prop_config, validation_type, value, subtype) {
+    if (!prop_config[validation_type]) return;
+    var valid_func, compare_to, interpolation_scope_extractor;
+    if (subtype) {
+      if (prop_config[validation_type][subtype] == undefined) return;
+      valid_func = valid_funcs[validation_type][subtype];
+      compare_to = prop_config[validation_type][subtype];
+      if (interpolation_scope_extractors[validation_type]) 
+        interpolation_scope_extractor = interpolation_scope_extractors[validation_type][subtype];
+    } else { 
+      valid_func = valid_funcs[validation_type];
+      compare_to = prop_config[validation_type];
+      interpolation_scope_extractor = interpolation_scope_extractors[validation_type];
+    }
+    if (!valid_func(value, compare_to)) {
+      var msg = prop_config.message;
+      if (subtype) {
+        msg = msg  || default_messages[validation_type][subtype] || 
+                global_default_messages[validation_type][subtype];
+      } else {
+        msg = msg  || default_messages[validation_type] || 
+                global_default_messages[validation_type];
+      }
+      var vars;
+      if (interpolation_scope_extractor)
+        vars = interpolation_scope_extractor(value);
+      errors.add(prop_name, interpolate_msg(msg, prop_name, value, compare_to, vars));
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  _.forEach(config.properties, function(prop_config, prop_name) {
+    var value = obj[prop_name];
+    if (prop_config.object) {
+      // recurse
+      errors.add(prop_name, validate(value, prop_config.object));
+      return; // no other validation types should apply if validation type is 'object'
+    }
+    for (var i = 0; i < validation_types.length; i++) {
+      var validation_type = validation_types[i];
+      if (!prop_config[validation_type]) continue;
+      if (typeof valid_funcs[validation_type] == 'function') {
+        var is_valid = test_and_add_error_message(prop_name, prop_config, 
+                         validation_type, value);
+        if (!is_valid && validation_type == 'required') break;
+      } else {
+        _.keys(valid_funcs[validation_type]).forEach(function(subtype) {
+          test_and_add_error_message(prop_name, prop_config, validation_type, 
+            value, subtype);
+        });        
+      }
+    }
+  });
+  if (!errors.is_empty()) return errors;
+}
+
+function interpolate_msg(msg, name, value, compare_to, vars) {
+  var interp_msg = msg.replace(/{{name}}/, name.humanize()).
+                       replace(/{{value}}/, value).
+                       replace(/{{compare_to}}/, compare_to);
+  if (vars)
+    _.forEach(vars, function(val, name) {
+      interp_msg = interp_msg.replace('{{'+name+'}}', val); 
+    });
+  return interp_msg;
+}
